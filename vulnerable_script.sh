1b12c13d19c46e6cee48c1993413ee4f6b4af03b
jessymcdermott <44265711+jessymcdermott@users.noreply.github.com>
2025-12-21 12:49:05 -0500

    Create vulnerable_script.sh

---
+++ vulnerable_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A typical insecure script runners might have
+apt-get update
+apt-get install -y netcat sudo
+touch /tmp/vuln.txt
+echo "Vulnerable container execution" > /tmp/vuln.txt
